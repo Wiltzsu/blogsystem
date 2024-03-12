370265990e2017d6959bdb146b8b394ae8e7192f
Wiltzsu <william.lonnberg@gmail.com>
2024-03-12 21:24:33 +0200

    create and register user

--- create_user.php
+++ create_user.php
@@ -20,18 +20,18 @@
         <div class="register-container">
             <div class="card p-4">
                 <h2 class="text-center mb-4">Register</h2>
-                <form>
+                <form method="post" action="register.php">
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" placeholder="Enter username">
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Enter password">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
                     </div>
                     <div class="form-group">
                         <label for="password">Re-enter password</label>
